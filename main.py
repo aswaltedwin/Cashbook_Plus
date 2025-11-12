0e9d1ee9730f2af04078d2a21eb8fce27a50076d
aswaltedwin <edwinaswalt06@gmail.com>
2025-11-12 10:42:35 +0530

    Ready for Vercel deployment

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable is missing")
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"}, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
